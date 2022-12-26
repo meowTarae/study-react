63e42efa0a3914462f62027b253350677019097d
타래 <85001878+meowTarae@users.noreply.github.com>
2022-12-26 23:07:49 +0900

    React movieApp ~#6.4
    React useEffect()의 cleanup fn에 대하여

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useEffect } from "react";
-import Button from "./Button";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Create! :D");
+    return () => console.log("Destroy! :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(true);
+  function onClick() {
+    setShowing((prev) => !prev);
+  }
   return (
     <>
-      <Button text={"Hello React!"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </>
   );
 }
