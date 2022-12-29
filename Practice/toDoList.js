760ad4331f9a90b3f537a90acab467c37ca23651
타래 <85001878+meowTarae@users.noreply.github.com>
2022-12-29 23:04:41 +0900

    Fixed ToDo list
    onClick event에서, 함수에 argument를 담아 보내려하니 자꾸 자동 실행되는 에러가 뜸..
    onClick={() => func(argu)} 처럼 event를 arrow func로 설정하면 에러가 해결이 됨.

---
+++ Practice/toDoList.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos((prevToDos) => [toDo, ...prevToDos]);
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+  };
+  return (
+    <>
+      <form>
+        <h1>My To Dos ({toDos.length})</h1>
+        <input
+          type="text"
+          placeholder="Input your to dos!"
+          value={toDo}
+          onChange={onChange}
+        />
+        <button type="submit" onClick={onSubmit}>
+          Add
+        </button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") return;
-    setToDo("");
-    setToDos((prevToDos) => [toDo, ...prevToDos]);
-  };
-  const deleteBtn = (index) => {
-    // setToDos((toDoArray) => toDoArray.filter((item, i) => i !== index));
-    // setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-    console.log("hello");
-  };
-  return (
-    <>
-      <form>
-        <h1>My To Dos ({toDos.length})</h1>
-        <input
-          type="text"
-          placeholder="Input your to dos!"
-          value={toDo}
-          onChange={onChange}
-        />
-        <button type="submit" onClick={onSubmit}>
-          Add
-        </button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={deleteBtn(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <h1>hi</h1>;
 }
 
 export default App;
