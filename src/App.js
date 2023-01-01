66d3e2808d767bad2ec5cbc2b38779f34945a1a3
타래 <85001878+meowTarae@users.noreply.github.com>
2023-01-01 23:53:14 +0900

    Create dollar to bitcoin converter
    달러를 입력하면 몇 비트코인인지 나타내줌

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [coinCost, setCost] = useState(0);
+  const [coinSymbol, setSymbol] = useState("");
   const [money, setMoney] = useState();
   const handleInputMoney = (event) => {
     setMoney(event.target.value);
-    let test = coins[0].quotes.USD.price;
-    console.log(test.toFixed(3));
   };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -17,39 +17,57 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const handleChangeCoins = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (value === "Select Coin !") {
+      setCost(0);
+      setSymbol("");
+      return;
+    }
+    setCost(JSON.parse(value).quotes.USD.price);
+    setSymbol(JSON.parse(value).symbol);
+  };
   return (
     <>
-      <h1>{loading ? "" : `We have ${coins.length} coins!`}</h1>
+      <h1>
+        {loading ? "" : `Dollar => coin ! ( we have ${coins.length} coins! )`}
+      </h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
         <div>
-          <label htmlFor="money">Input how much dollar do you have!</label>
-          <br />
+          <h3>Input how much dollar do you have !</h3>
           <input
             id="money"
             type="number"
             placeholder="Enter the money you have"
             onChange={handleInputMoney}
           />
+          <label htmlFor="money">Dollar </label>
           <br />
-
-          <select>
+          <select onChange={handleChangeCoins}>
+            <option>Select Coin !</option>
             {coins.map((coin, index) => (
-              <option key={index}>
-                {coin.name} : {coin.quotes.USD.price}
+              <option key={index} value={JSON.stringify(coin)}>
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
               </option>
             ))}
           </select>
+          <br />
+          {money ? (
+            <h3>
+              {coinCost === 0
+                ? "Plz select coins !"
+                : ` => ${money / coinCost} ${coinSymbol}`}
+            </h3>
+          ) : (
+            " "
+          )}
         </div>
       )}
     </>
   );
 }
 export default App;
-
-/*
-  ̶1̶.̶ ̶R̶e̶c̶e̶i̶v̶e̶ ̶i̶n̶p̶u̶t̶ ̶f̶r̶o̶m̶ ̶t̶h̶e̶ ̶u̶s̶e̶r̶ ̶o̶f̶ ̶t̶h̶e̶ ̶d̶o̶l̶l̶a̶r̶ ̶t̶h̶e̶y̶ ̶c̶u̶r̶r̶e̶n̶t̶l̶y̶ ̶h̶a̶v̶e̶.̶
-  2. option에서 코인을 선택한다.
-  3. 현재 달러로 해당 코인을 얼마나 살 수 있는지 보여준다.
-*/
