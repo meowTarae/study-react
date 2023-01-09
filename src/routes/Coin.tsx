f18127d8c64f43468c5b39a5bd05e1783fd3c8a1
타래 <85001878+meowTarae@users.noreply.github.com>
2023-01-09 22:12:32 +0900

    React master ~#5.12

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import {
   Switch,
@@ -197,7 +196,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
