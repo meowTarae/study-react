42019bc0ef95580015e0bda895fa22798507e600
타래 <85001878+meowTarae@users.noreply.github.com>
2023-01-10 23:13:34 +0900

    React master ~#5.13
    Using APEXCHART

--- src/api.ts
+++ src/api.ts
@@ -16,10 +16,11 @@ export function fetchCoinTickers(coinId: string) {
   );
 }
 
-export function fetchCoinHistory(coinId: string) {
+export async function fetchCoinHistory(coinId: string) {
   const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
-  return fetch(
+  const startDate = endDate - 60 * 60 * 12 * 1;
+  const response = await fetch(
     `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
-  ).then((response) => response.json());
+  );
+  return await response.json();
 }
