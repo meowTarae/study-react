e0e7dd5e3193f92c54c4b7fc7da1ea9cdf33a189
타래 <85001878+meowTarae@users.noreply.github.com>
2023-01-15 23:46:25 +0900

    Styling screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
   <>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </>
 );
 
