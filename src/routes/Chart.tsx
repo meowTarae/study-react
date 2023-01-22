b02a29d529234e52a1608bb01154f0444e1d9e61
타래 <85001878+meowTarae@users.noreply.github.com>
2023-01-22 23:32:03 +0900

    Create chart
    코드 챌린지: x축에 날짜 기입하기 O

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexCharts from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
@@ -8,16 +9,54 @@ interface ChartProps {
 interface dataProps {
   time_open: number;
   time_close: number;
-  open: number;
-  high: number;
-  low: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery<dataProps>(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<dataProps[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <>{isLoading ? "Now Loading..." : <ul></ul>}</>;
+  return (
+    <>
+      {isLoading ? (
+        "Now Loading..."
+      ) : (
+        <ApexCharts
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            chart: {
+              type: "line",
+              height: "100%",
+            },
+            theme: {
+              mode: "dark",
+            },
+            stroke: {
+              width: 5,
+              curve: "smooth",
+            },
+            xaxis: {
+              type: "datetime",
+              categories: data?.map((time) => {
+                let closeTime = new Date(time.time_close * 1000);
+                return closeTime.toLocaleDateString("en-US");
+              }),
+            },
+          }}
+        />
+      )}
+    </>
+  );
 }
 
 export default Chart;
