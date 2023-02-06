d5db0b62afcef2afcb66937acc6e1aa7762594ce
타래 <85001878+meowTarae@users.noreply.github.com>
2023-02-06 15:46:31 +0900

    ApexChart 타입 지정하는 방법
    Options-chart-type이 아니라 가장 상위 블록에 type지정해줘야됨

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query";
 import { fetchCoinPrice } from "../api";
-
+import ApexCharts from "react-apexcharts";
 interface PriceProps {
   coinId: string;
 }
@@ -13,7 +13,37 @@ function Price({ coinId }: PriceProps) {
       refetchInterval: 10000,
     }
   );
-  return <h1>price tab!</h1>;
+
+  return (
+    <>
+      {isLoading ? (
+        "Now Loading..."
+      ) : (
+        <ApexCharts
+          type="candlestick"
+          series={[
+            {
+              data: [
+                {
+                  x: new Date(1538778600000),
+                  y: [6629.81, 6650.5, 6623.04, 6633.33],
+                },
+                {
+                  x: new Date(1538780400000),
+                  y: [6632.01, 6643.59, 6620, 6630.11],
+                },
+              ],
+            },
+          ]}
+          options={{
+            chart: {
+              height: 300,
+            },
+          }}
+        />
+      )}
+    </>
+  );
 }
 
 export default Price;
