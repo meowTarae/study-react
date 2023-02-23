46261f384bd112510156e930d825db6dbcc059ba
타래 <85001878+meowTarae@users.noreply.github.com>
2023-02-24 07:29:23 +0900

    Add basename={process.env.PUBLIC_URL}
    깃허브에 배포 했을 때, 빈 화면만 나온다면
    베이스 루트를 추가해주자

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
-  <>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </>
+  </BrowserRouter>
 );
